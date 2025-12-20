c380d78f90f120f363b7e228f9a12135d119f725
Writingway <sendmeat4time@gmail.com>
2025-12-20 20:07:57 +0100

    fix: streamline environment variable handling in main and find_path functions

--- main.c
+++ main.c
@@ -29,18 +29,15 @@ void display_prompt(void)
 int main(int argc, char **argv, char **envp)
 {
 	char *input;
-	char **g_env;
 
 	(void)argc;
 	(void)argv;
-
-	g_env = envp;
 
 	while (1)
 	{
 		display_prompt();
 		input = read_input();
-		execute_command(input, g_env);
+		execute_command(input, envp);
 		free(input);
 	}
 	return (0);
