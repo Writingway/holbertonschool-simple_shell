fcc79c42a7a02e61aaa3add90395683c8f201e15
Writingway <sendmeat4time@gmail.com>
2025-12-20 19:30:48 +0100

    fix: refactor execute_command to use find_path and streamline error handling

--- execute.c
+++ execute.c
@@ -7,34 +7,22 @@
  */
 void execute_command(char *line, char **env)
 {
-	char **argv;
+	char **argv, *cmd_path;
 	pid_t pid;
 	int status;
-	char *cmd_path;
 
-	/*Split the line with split_line function */
 	argv = split_line(line);
-	/*Check if argv is NULL or the first argument is NULL */
 	if (argv == NULL || argv[0] == NULL)
 	{
 		free(argv);
 		return;
 	}
-<<<<<<< HEAD
 	cmd_path = find_path(argv[0], env);
 	if (cmd_path == NULL)
 	{
 		fprintf(stderr, "%s: command not found\n", argv[0]);
 		free(argv);
 		return;
-=======
-	cmd_path = find_command(argv[0], env);
-	if (!cmd_path)
-	{
-		fprintf(stderr, "%s: command not found\n", argv[0]);
-		free(argv);
-		return; /* pas de fork */
->>>>>>> 7c246778a6d2622781c67dfb6b87e67f5b7dff48
 	}
 	pid = fork();
 	if (pid == -1)
@@ -53,11 +41,9 @@ void execute_command(char *line, char **env)
 			free(cmd_path);
 			exit(127);
 		}
+		return;
 	}
-	else
-	{
-		waitpid(pid, &status, 0);
-		free(argv);
-		free(cmd_path);
-	}
+	waitpid(pid, &status, 0);
+	free(argv);
+	free(cmd_path);
 }
