1382a7e863efc76f0beec3a54ad4fbf05fcb9eac
LucasN <lucas.n230896@gmail.com>
2025-12-22 14:00:12 +0000

    fix: when user types exit on a shell, it exit and don't fork

--- execute.c
+++ execute.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * buildtin_exit - exit shell if user types "exit"
+ * @argv: arguments vector
+ * Return: 
+ */
+void buildtin_exit(char **argv)
+{
+	int exit_status = 0;
+
+	if (strcmp(argv[0], "exit") != 0)
+		return;
+
+	if (argv[1])
+		exit_status = atoi(argv[1]);
+
+	free_argv(argv);
+	exit(exit_status);
+
+}
+
+/**
  * execute_command - Executes a command
  * @line: The command line to execute
  * @env: The environment variables
@@ -17,6 +37,8 @@ int execute_command(char *line, char **env, char *prog_name, int line_number)
 
 	if (!argv || !argv[0])
 		return (free_argv(argv), 0);
+	/* buildtin exit avant de fork */
+	buildtin_exit(argv);
 
 	cmd_path = find_path(argv[0], env);
 	if (!cmd_path)
--- main.h
+++ main.h
@@ -19,6 +19,7 @@ char *read_input(void);
 char **split_line(char *line);
 int count_arguments(char *line);
 int execute_command(char *line, char **env, char *prog_name, int line_number);
+void buildtin_exit(char **argv);
 char *get_path(char **env);
 char *find_path(char *cmd, char **env);
 void free_argv(char **argv);
