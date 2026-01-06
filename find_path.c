ebbd6d6b942b0ab3d2097b39575cca2eb011dd07
Writingway <sendmeat4time@gmail.com>
2026-01-06 18:47:00 +0100

    fix: update comments in find_path function for clarity and consistency

--- find_path.c
+++ find_path.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * find_path - Cherche le chemin complet d'une commande
- * @cmd: commande entrée
- * @env: variables d'environnement
- * Return: chemin complet de la commande si trouvée, NULL sinon
+ * find_path - Find the full path of a command in the PATH environment variable
+ * @cmd: the command to find
+ * @env: environment variables
+ * Return: full path of the command if found, NULL otherwise
  */
 char *find_path(char *cmd, char **env)
 {
